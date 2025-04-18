3f20c2271010a3ffb2056d04cfcea3569c450b2b
Yves Talboom <yves.talboom@gmail.com>
2025-04-18 10:40:35 +0200

    fix top border authentication

--- client/src/app/authProvider.tsx
+++ client/src/app/authProvider.tsx
@@ -43,7 +43,7 @@ const formFields = {
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="mt-5">
+    <div>
       <Authenticator formFields={formFields}>
         {({ user }: any) =>
           user ? (
